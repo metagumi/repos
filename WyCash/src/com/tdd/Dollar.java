8002870438b3f958de391e8a785e46f5b38b22c6
Sylvester <metagumi@outlook.com>
2017-03-04 08:18:10 -0500

    add Dollar class

---
+++ WyCash/src/com/tdd/Dollar.java
@@ -0,0 +1,14 @@
+package com.tdd;
+
+/**
+ * Created by Sylvester on 3/4/2017.
+ */
+public class Dollar {
+    int amount;
+    Dollar(int amount) {
+
+    }
+    void times(int multiplier) {
+
+    }
+}
