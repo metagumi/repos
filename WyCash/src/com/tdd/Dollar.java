42708bf9eb04b100b12124a3f427f99c52273911
Sylvester <metagumi@outlook.com>
2017-03-04 09:47:26 -0500

    testMultiplication updated

--- WyCash/src/com/tdd/Dollar.java
+++ WyCash/src/com/tdd/Dollar.java
@@ -6,9 +6,10 @@ package com.tdd;
 public class Dollar {
     int amount;
     Dollar(int amount) {
+        this.amount = amount;
 
     }
     void times(int multiplier) {
-
+        amount *= multiplier;
     }
 }
--- WyCash/src/com/tdd/DollarTest.java
+++ WyCash/src/com/tdd/DollarTest.java
@@ -1,8 +1,18 @@
 package com.tdd;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 /**
  * Created by Sylvester on 3/4/2017.
  */
+@Test
 public class DollarTest {
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
 
 }
