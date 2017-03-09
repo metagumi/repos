3815a23d14c9953b4df7025e2f50bfc5199e0c0e
sylvesterharvey <metagumi@gmail.com>
2017-03-08 21:00:02 -0500

    almost ready to push times to Money

--- WyCash/src/com/tdd/Money.java
+++ WyCash/src/com/tdd/Money.java
@@ -3,11 +3,28 @@ package com.tdd;
 /**
  * Created by sylvesterharvey on 3/7/17.
  */
-public class Money {
+abstract class Money {
+    //abstract String currency();
+    abstract Money times(int multiplier);
     protected int amount;
+    protected String currency;
+    
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+    static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+    static Money franc(int amount) {
+        return new Franc(amount,"CHF");
+    }
+    String currency() {
+        return currency;
     }
 }
